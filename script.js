c448f31c9a137a63434e8721c79fbfe8b9736915
noble0423 <ryan.noble82@gmail.com>
2020-02-06 15:52:31 -0600

    added function to ask user to confirm elements to be inc;uded in the password generator.

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-  // series of questions (prompts) for password criteria that user selects whether to include in the password generator or not
-    // password length prompt (must be at least 8 characters, no more than 128). 
-    // validate selection
-    
-    // char type prompts (lowercase, uppercase, numeric, and/or special characters). 
-    // validate selections
+// GLOBAL VARIABLES
 
-    // include atleast 1 of ea type of char type that user selected
+let promptPasswordLength;
+let confirmCharLowercase;
+let confirmCharUppercase;
+let confirmCharNumeric;
+let confirmCharSpecial;
 
-  // after all selections have been answered, then the password is generated that matches the selected criteria
-
-    // display password in alert or on the DOM
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+
+// FUNCTIONS
+
+// User Prompts Function
+function askUserQuestions() {
+
+  promptPasswordLength = prompt("How many characters long do you want your password to be?");
+
+  confirmCharLowercase = confirm("Would you like to inlude lowercase letters in your password?");
+
+  confirmCharUppercase = confirm("Would you like to include uppercase letters in your password?");
+
+  confirmCharNumeric = confirm("would you like to include numeric characters in your password?");
+
+  confirmCharSpecial = confirm("Would you like to include special characters in your password?");
+  
+}
 
 // Generate Password Function
 function generatePassword() {
@@ -29,5 +42,42 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// LOGIC
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+
+  askUserQuestions();
+
+  console.log(`length: ${promptPasswordLength}`);
+  console.log(`lowercase: ${confirmCharLowercase}`);
+  console.log(`uppercase: ${confirmCharUppercase}`);
+  console.log(`numeric: ${confirmCharNumeric}`);
+  console.log(`special: ${confirmCharSpecial}`);
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
+
+
+
+});
+// series of questions (prompts) for password criteria that user selects whether to include in the password generator or not
+  // password length prompt (must be at least 8 characters, no more than 128). 
+  // validate selection
+  
+  // char type prompts (lowercase, uppercase, numeric, and/or special characters). 
+  // validate selections
+
+  // include atleast 1 of ea type of char type that user selected
+
+// after all selections have been answered, then the password is generated that matches the selected criteria
+
+  // display password in alert or on the DOM
+
+
+
+
+
+
+
